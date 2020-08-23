b2c38aaea42752dc61145bf88448a05ebc65aeaa
Marlon Castano <mscastanom@correo.udistrital.edu.co>
2020-08-22 22:09:44 -0500

    se agrego clase producto

---
+++ FinalProject/src/java/Modelo/DTO/Producto.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.DTO;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author ASUS
+ */
+@Named(value = "producto")
+@Dependent
+public class Producto {
+
+    /**
+     * Creates a new instance of Producto
+     */
+    public Producto() {
+    }
+    
+}
