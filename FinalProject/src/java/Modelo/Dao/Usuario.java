385f3f515e16e592e9a119498a859e15c25e64c2
Marlon Castano <mscastanom@correo.udistrital.edu.co>
2020-08-22 22:00:05 -0500

    se agrego Modelo Dao

---
+++ FinalProject/src/java/Modelo/Dao/Usuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Dao;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author ASUS
+ */
+@Named(value = "usuario")
+@Dependent
+public class Usuario {
+
+    /**
+     * Creates a new instance of Usuario
+     */
+    public Usuario() {
+    }
+    
+}
