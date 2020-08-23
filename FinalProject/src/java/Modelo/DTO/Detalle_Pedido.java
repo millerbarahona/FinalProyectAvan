9fcdc07dfe1d07fc0e508cad45bb6095680247cf
miller barahona <millermiller01@hotmail.com>
2020-08-22 22:55:28 -0500

    Pedido 1

--- FinalProject/src/java/Modelo/DTO/Detalle_Pedido.java
+++ FinalProject/src/java/Modelo/DTO/Detalle_Pedido.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo.DTO;
 
-/**
- *
- * @author Usuario
- */
-public class Detalle_Pedido {
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+public class Detalle_Pedido implements Serializable{
+        
+    int id_pedido,cant_und;
+    List<Producto>productos;
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public void setId_pedido(int id_pedido) {
+        this.id_pedido = id_pedido;
+    }
+
+    public int getCant_und() {
+        return cant_und;
+    }
+
+    public void setCant_und(int cant_und) {
+        this.cant_und = cant_und;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
     
 }
--- FinalProject/src/java/Modelo/DTO/Pedido.java
+++ FinalProject/src/java/Modelo/DTO/Pedido.java
@@ -12,6 +12,47 @@ public class Pedido implements Serializable{
     int id_pedido, id_usu, valor_total;
     String observaciones, estado;
     
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public void setId_pedido(int id_pedido) {
+        this.id_pedido = id_pedido;
+    }
+
+    public int getId_usu() {
+        return id_usu;
+    }
+
+    public void setId_usu(int id_usu) {
+        this.id_usu = id_usu;
+    }
+
+    public int getValor_total() {
+        return valor_total;
+    }
+
+    public void setValor_total(int valor_total) {
+        this.valor_total = valor_total;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
     
 }
