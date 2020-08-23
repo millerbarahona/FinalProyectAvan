9f65da4f1dfe398027b48bb168dc51605ed83f8e
miller barahona <millermiller01@hotmail.com>
2020-08-22 22:22:06 -0500

    Usuario bien

--- FinalProject/src/java/Modelo/DTO/Usuario.java
+++ FinalProject/src/java/Modelo/DTO/Usuario.java
@@ -11,5 +11,69 @@ public class Usuario {
     String nombres, apellidos, tipo_docto,numero_docto,ciudad_residencia,direccion,rol;
     public Usuario() {
     }
+
+    public int getId_usu() {
+        return id_usu;
+    }
+
+    public void setId_usu(int id_usu) {
+        this.id_usu = id_usu;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTipo_docto() {
+        return tipo_docto;
+    }
+
+    public void setTipo_docto(String tipo_docto) {
+        this.tipo_docto = tipo_docto;
+    }
+
+    public String getNumero_docto() {
+        return numero_docto;
+    }
+
+    public void setNumero_docto(String numero_docto) {
+        this.numero_docto = numero_docto;
+    }
+
+    public String getCiudad_residencia() {
+        return ciudad_residencia;
+    }
+
+    public void setCiudad_residencia(String ciudad_residencia) {
+        this.ciudad_residencia = ciudad_residencia;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
     
 }
